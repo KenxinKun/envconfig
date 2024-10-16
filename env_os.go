3648badf0143da1c3438bdcdfc84e7364ff77d35
Borja Lazaro Toralles <borjalazarotoralles@gmail.com>
2024-10-16 13:19:04 +0100

    add-SetLookupEnv

--- env_os.go
+++ env_os.go
@@ -1,3 +1,4 @@
+//go:build appengine || go1.5
 // +build appengine go1.5
 
 package envconfig
@@ -5,3 +6,7 @@ package envconfig
 import "os"
 
 var lookupEnv = os.LookupEnv
+
+func SetLookupEnv(f func(key string) (string, bool)) {
+	lookupEnv = f
+}
